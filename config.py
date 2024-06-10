b90383fe26697c6f2896a63bc3180419a22ac49a
진원규 <lala098917@gmail.com>
2024-06-10 21:21:54 +0900

    message

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-# os는 시스템 모듈, 기능이 많음
-
-BASE_DIR = os.path.dirname(__file__)
-#URI는 데이터베이스 접속 주소.
-SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'board.db')) # board.db는 db명임, 디비명을 설정한것
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = "dev"
--- config/development.py
+++ config/development.py
@@ -3,4 +3,3 @@ from config.default import *
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR,'board.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = "dev"
-
